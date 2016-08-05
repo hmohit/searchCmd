6e80c7b543d3192f3cc7783c603d341ec06e4a9c
ankit-gupta1 <ankit.ramesh.gupta@gmail.com>
2016-08-04 22:32:15 -0700

    create bare minimum search interface

--- searchCmdApp.py
+++ searchCmdApp.py
@@ -1,14 +1,32 @@
 from searchMetaData import SearchMetaData
 from argparse import ArgumentParser
+from pickle import dump, load
 
 
 def main():
+    parser = ArgumentParser()
+    parser.add_argument('-fname', '--file-name', required=False, help='file path to command list')
+    parser.add_argument('-s', '--search', required=False, help='enter search string')
+
     try:
-        parser = ArgumentParser()
-        parser.add_argument('-fname', '--file-name', required=True)
         args = vars(parser.parse_args())
-        delegate = SearchMetaData()
-        delegate.load_new_commands(filename=args['file_name'])
+
+        try:
+            delegate = load(open('metadata.raw', 'rb'))
+
+        except Exception as err:
+            print err
+            delegate = SearchMetaData()
+
+        if args['file_name'] is not '':
+            delegate.load_new_commands(filename=args['file_name'])
+            dump(delegate, open('metadata.raw', 'wb'))
+
+        elif args['search'] is not '':
+            print 'We have following commands for your requested search ' + args['search']
+
+            for cmd in delegate.search(args['search']):
+                print cmd
 
     except Exception as err:
         print err
