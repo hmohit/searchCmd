c2e728dca2c9fc9a8e5e4f0a789df72e81a19308
Ankit Gupta <agupta@zenefits.com>
2016-08-09 22:22:09 -0700

    add dict method for search

--- searchCmdApp.py
+++ searchCmdApp.py
@@ -7,6 +7,7 @@ def main():
     parser = ArgumentParser()
     parser.add_argument('-f', '--file-name', required=False, help='file path to command list')
     parser.add_argument('-s', '--search', required=False, help='enter search string')
+    parser.add_argument('-sd', '--search-dict', required=False, help='enter search string')
 
     try:
         args = vars(parser.parse_args())
@@ -20,12 +21,19 @@ def main():
 
         if args['file_name'] is not '':
             delegate.load_new_commands(filename=args['file_name'])
+            delegate.create_dict()
             dump(delegate, open('metadata.raw', 'wb'))
 
         elif args['search'] is not '':
             print 'We have following commands for your requested search ' + args['search']
 
             for cmd in delegate.search(args['search']):
+                print cmd
+
+        elif args['search_dict'] is not '':
+            print 'We have following commands for your requested search ' + args['search_dict']
+
+            for cmd in delegate.search_dict(args['search_dict']):
                 print cmd
 
     except Exception as err:
--- searchMetaData.py
+++ searchMetaData.py
@@ -1,6 +1,10 @@
+from collections import defaultdict
+
+
 class SearchMetaData:
     def __init__(self):
         self.command_list = []
+        self.command_dict = defaultdict(list)
 
     def load_new_commands(self, filename):
         with open(name=filename) as file_handle:
@@ -13,3 +17,13 @@ class SearchMetaData:
                 result.append(cmd)
 
         return result
+
+    def create_dict(self):
+        for cmd in self.command_list:
+            cmd_length = len(cmd)
+            for start in xrange(cmd_length):
+                for end in xrange(start, cmd_length):
+                    self.command_dict[cmd[start:(end + 1)]].append(cmd)
+
+    def search_dict(self, search_str):
+        return self.command_dict[search_str]
