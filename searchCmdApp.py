db4ee1c9abb8ce7c322e437e7ef857571423097e
Mohit <mhotwani@cisco.com>
2016-09-13 23:25:02 -0700

    changed the delete command

--- searchCmdApp.py
+++ searchCmdApp.py
@@ -12,8 +12,7 @@ def main():
     parser.add_argument('-S', '--searchDict', help='enter search string')
     mutex_group1= parser.add_mutually_exclusive_group()
     mutex_group1.add_argument('-a', '--add', help='enter command to be added')
-    mutex_group1.add_argument('-d', '--delete', action='store_true',
-                              help='enter command to be added')
+    mutex_group1.add_argument('-d', '--delete', help='enter command to be deleted')
     meta_file = os.path.join(os.path.dirname(__file__), 'metadata.raw')
     try:
         args = parser.parse_args()
@@ -29,12 +28,13 @@ def main():
             dump(delegate, open(meta_file, 'wb'))
 
         elif args.delete:
-            results = delegate.search(args.search)
+            results = delegate.search(args.delete)
             for k,v  in enumerate(results):
                 print k, ':', v
-            num = int(raw_input('Delete cmd num: '))
-            delegate.delete(results[num])
-            dump(delegate, open(meta_file, 'wb'))
+            if results:
+                num = int(raw_input('Delete cmd num: '))
+                delegate.delete(results[num])
+                dump(delegate, open(meta_file, 'wb'))
 
         elif args.search:
             for cmd in delegate.search(args.search):
