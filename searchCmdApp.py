40477d3bf17f9b0d10594030ff6be0f470e62b7e
Ankit Gupta <agupta@zenefits.com>
2016-09-15 20:51:55 -0700

    now you can add tags for your strings along with your command

--- searchCmdApp.py
+++ searchCmdApp.py
@@ -8,37 +8,31 @@ import os
 def main():
     parser = ArgumentParser(description='Search commands from database')
     parser.add_argument('-f', '--filename', required=False, help='file path to command list')
-    parser.add_argument('-s', '--search', required=False, help='enter search string')
-    parser.add_argument('-S', '--searchDict', required=False, help='enter '
-                                                                   'search string')
-    parser.add_argument('-a', '--add', required=False, help='enter command to'
-                                                            'be added')
+    parser.add_argument('-s', '--search', required=False, type=str, help='enter search string')
+    parser.add_argument('-a', '--add', required=False, nargs='+', type=str,
+                        help='enter commands followed by optional tags')
     meta_file = os.path.join(os.path.dirname(__file__), 'metadata.raw')
+
     try:
         args = parser.parse_args()
 
         try:
             delegate = load(open(meta_file, 'rb'))
 
-        except Exception:
+        except Exception as err:
+            print err
             delegate = SearchMetaData()
+
         if args.filename:
             delegate.load_new_commands(filename=args.filename)
-            delegate.create_dict()
             dump(delegate, open(meta_file, 'wb'))
 
         elif args.search:
             for cmd in delegate.search(args.search):
                 print cmd
 
-        elif args.searchDict:
-            print 'We have following commands for your requested search ' + args.searchDict
-
-            for cmd in delegate.search_dict(args.searchDict):
-                print cmd
-
         elif args.add:
-            delegate.add_command(args.add)
+            delegate.add_command(args.add[0], set(args.add))
             dump(delegate, open(meta_file, 'wb'))
 
     except Exception as err:
--- searchMetaData.py
+++ searchMetaData.py
@@ -1,40 +1,51 @@
 from __future__ import division
-from collections import defaultdict
 from fuzzywuzzy import process
+
+
+class CommandMetaData:
+    def __init__(self, command_str=None, command_tags=set()):
+        self.command_str = command_str
+        self.command_tags = command_tags
+
+    def is_search_str_in_tags(self, search_str):
+        result = False
+        for tag in self.command_tags:
+            result = result or search_str in tag
+            if result is True:
+                break
+
+        return result
 
 
 class SearchMetaData:
     def __init__(self):
         self.command_list = set()
-        self.command_dict = defaultdict(list)
 
     def load_new_commands(self, filename):
         with open(name=filename) as file_handle:
-            self.command_list += set(file_handle.readlines())
+            for cmd in set(file_handle.readlines()):
+                cmd = cmd.rstrip()
+                self.command_list.add(CommandMetaData(command_str=cmd, command_tags={cmd}))
 
-    def add_command(self, cmd):
-        self.command_list.add(cmd)
-
-    def create_dict(self):
-        for cmd in self.command_list:
-            cmd_length = len(cmd)
-            for start in xrange(cmd_length):
-                for end in xrange(start, cmd_length):
-                    self.command_dict[cmd[start:(end + 1)]].append(cmd)
+    def add_command(self, cmd, tags):
+        self.command_list.add(CommandMetaData(command_str=cmd, command_tags=tags))
 
     def search(self, search_str):
-        result = [cmd for cmd in self.command_list if search_str in cmd]
+        result = [cmd.command_str for cmd in self.command_list if cmd.is_search_str_in_tags(search_str)]
         if len(result) is 0:
             result = self.search_cmd_approx(search_str)
 
-        return result
-
-    def search_dict(self, search_str):
-        if search_str in self.command_dict.keys():
-            return self.command_dict[search_str]
-        else:
-            return self.search_cmd_approx(search_str)
+        return result[:5]
 
     def search_cmd_approx(self, search_str):
-        approx_matches = process.extract(query=search_str, choices=self.command_list, limit=5)
-        return [k for k,v in approx_matches if v > 50]
+        approx_matches = []
+        for cmd in self.command_list:
+            matches = process.extract(query=search_str, choices=cmd.command_tags, limit=1)
+            if matches and matches[0][1] > 50:
+                approx_matches.append((cmd.command_str, matches[0][1]))
+
+        approx_matches.sort(key=lambda x: x[1], reverse=True)
+        if approx_matches:
+            return zip(*approx_matches)[0]
+        else:
+            return approx_matches
